414f68bee2189550ba53879ddb82de5cc67b99b1
Jared-Bottelsen <64649502+Jared-Bottelsen@users.noreply.github.com>
2022-01-06 21:23:42 -0700

    Remove unused code

--- src/app/components/header/header-controller.js
+++ src/app/components/header/header-controller.js
@@ -1,28 +1,11 @@
 angular.module('appModule')
 .controller('headerCtrl', [
     '$scope', 
-    '$http',
+    'ApiService',
     AppHeaderController
 ]);
 
-function AppHeaderController($scope, $http) {
+function AppHeaderController($scope, ApiService) {
     $scope.test = 'Hello World';
-    $scope.drinks = [];
-
-    $scope.getData = function() {
-        $http({
-            method: 'GET',
-            url: 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-        })
-        .then(function(response) {
-            console.log(response);
-            $scope.drinks = response.data;
-        })
-        .catch(function(error) {
-            console.error(error);
-        })
-    }
-
-    // $scope.getData();
-
+    ApiService.getRandomCocktail();
 }
